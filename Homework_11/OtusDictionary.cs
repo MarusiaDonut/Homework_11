7eb8415a855d9f2110fa9b710ff974d286648966
MYudina <MYudina@PC_MSK-20470>
2023-09-04 18:20:25 +0300

    Add project files.

---
+++ Homework_11/OtusDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+
+    internal class OtusDictionary 
+    {
+        public int Key { get; set; }
+        public string? Value { get; set; }
+
+
+        string[] array = new string[32];
+        public void Add(int key, string value)
+        {
+            int keyContainer = key;
+            while (true)
+            {
+                if (value != null)
+                {
+                    key = Math.Abs(key % array.Length);
+                    if (array[key] == null)
+                    {
+                       array[key] = value;
+                    }
+                    else
+                    {
+                        string[] arrayContainer = array;
+                        Array.Resize(ref array, arrayContainer.Length * 2);
+                        for (int i = 0; i < arrayContainer.Length; i++)
+                        {
+                            key = i;
+                            key = Math.Abs(key % array.Length);
+                            array[keyContainer] = value;
+                        }
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                break;
+            }
+        }
+
+        public string Get(int key)
+        {
+                return array[key];
+        }
+
+    }
+}
---
+++ Homework_11/Program.cs
@@ -0,0 +1,14 @@
+﻿using Homework_11;
+
+Random rnd = new Random();
+var t = new OtusDictionary() {Key = 1, Value = "hello" };
+OtusDictionary otusDictionary = new OtusDictionary();
+
+for (int i = 0; i < 32; i++)
+{
+    otusDictionary.Add(rnd.Next(0, 32), "Элемент " + i);
+}
+
+Console.WriteLine(otusDictionary.Get(2));
+Console.ReadLine();
+
