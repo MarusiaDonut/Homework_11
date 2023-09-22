825ba6e1984bca97374dde7f0bd47415fa893847
MYudina <MYudina@PC_MSK-20470>
2023-09-22 17:21:21 +0300

    испарвление

--- Homework_11/OtusDictionary.cs
+++ Homework_11/OtusDictionary.cs
@@ -8,49 +8,46 @@ using System.Threading.Tasks;
 namespace Homework_11
 {
 
-    internal class OtusDictionary 
+    internal class OtusDictionary
     {
-        public int Key { get; set; }
-        public string? Value { get; set; }
-
-
         string[] array = new string[32];
         public void Add(int key, string value)
         {
             int keyContainer = key;
-            while (true)
+
+            if (value != null)
             {
-                if (value != null)
+                key = Math.Abs(key % array.Length);
+                if (array[key] == null)
                 {
-                    key = Math.Abs(key % array.Length);
-                    if (array[key] == null)
-                    {
-                       array[key] = value;
-                    }
-                    else
-                    {
-                        string[] arrayContainer = array;
-                        Array.Resize(ref array, arrayContainer.Length * 2);
-                        for (int i = 0; i < arrayContainer.Length; i++)
-                        {
-                            key = i;
-                            key = Math.Abs(key % array.Length);
-                            array[keyContainer] = value;
-                        }
-                        break;
-                    }
+                    array[key] = value;
                 }
                 else
                 {
-                    break;
+                    string[] arrayContainer = array;
+                    Array.Resize(ref array, arrayContainer.Length * 2);
+                    for (int i = 0; i < arrayContainer.Length; i++)
+                    {
+                        key = Math.Abs(i % array.Length);
+                        
+                        array[key] = array[i];
+                    }
+                    keyContainer = Math.Abs(keyContainer % array.Length);
+                    array[keyContainer] = value;
                 }
-                break;
+            }
+            else
+            {
+                throw new Exception("Ошибка!");
             }
         }
 
         public string Get(int key)
         {
+            if (array[key] != null)
                 return array[key];
+            else
+                throw new Exception();
         }
 
     }
--- Homework_11/Program.cs
+++ Homework_11/Program.cs
@@ -1,14 +1,14 @@
 ﻿using Homework_11;
 
 Random rnd = new Random();
-var t = new OtusDictionary() {Key = 1, Value = "hello" };
 OtusDictionary otusDictionary = new OtusDictionary();
 
-for (int i = 0; i < 32; i++)
+for (int i = 0; i < 33; i++)
 {
-    otusDictionary.Add(rnd.Next(0, 32), "Элемент " + i);
+    otusDictionary.Add(rnd.Next(0, 10000), "Элемент " + i);
 }
 
-Console.WriteLine(otusDictionary.Get(2));
+Console.WriteLine(otusDictionary.Get(10));
+
 Console.ReadLine();
 
